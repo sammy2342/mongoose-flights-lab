6bb96a97178ae9f1b2b7a29a1ff4cbbd39d38e47
sammyfares <sammyfares2003@gmail.com>
2022-10-03 21:08:17 -0400

    complete sort bounes

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -7,8 +7,9 @@ module.exports = {
 function create(req, res) { 
     console.log(req.body, 'this is a console.log')
     Flight.findById(req.params.id, function(err, flight) { 
+        console.log(flight.destinations, 'adslfmalsdfmlksadfmmasd;flmk')
         flight.destinations.push(req.body)
-        console.log(flight.destinations)
+        flight.destinations.sort( (a, b) => b.arrival - a.arrival)
         flight.save( function(err) { 
             res.redirect(`/flights/${flight.id}`)
         })
